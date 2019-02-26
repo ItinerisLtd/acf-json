e3e1d1d476d97876be34e3ea9aefc915d021da7f
Tang Rufus <tangrufus@gmail.com>
2019-02-26 11:15:55 +0000

    Extract from PhySoc

---
+++ acf-json.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name:     Itineris ACF JSON
+ * Plugin URI:      https://github.com/itinerisltd/acf-json
+ * Description:     Load ACF JSON settings from <bedrock>/config/acf-json directory
+ * Version:         0.1.0
+ * Author:          Itineris Limited
+ * Author URI:      https://www.itineris.co.uk/
+ * Text Domain:     acf-json
+ */
+
+declare(strict_types=1);
+
+// If this file is called directly, abort.
+if (! defined('WPINC')) {
+    die;
+}
+
+// Something is wrong, abort.
+if (! defined('ABSPATH')) {
+    die;
+}
+
+add_filter('acf/settings/save_json', function (): string {
+    return ABSPATH . '../../config/acf-json';
+}, 10000);
+
+add_filter('acf/settings/load_json', function (array $paths): array {
+    return array_merge($paths, [
+        ABSPATH . '../../config/acf-json',
+    ]);
+}, 10000);
